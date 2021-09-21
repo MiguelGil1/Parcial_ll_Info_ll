a3eea1dcd77754df6ed4950c8a220eb936eaa2fc
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-09-20 21:06:23 -0500

    Se agrega codigo fuente de las pruebas que se le ha hecho al circuito:

--- Tinkercad/parcial_ll_informatica_ll1.ino
+++ Tinkercad/parcial_ll_informatica_ll1.ino
@@ -1,18 +1,31 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN 2
-#define LED_COUNT 200
+#define LED_COUNT 256
 
 Adafruit_NeoPixel leds(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // C++ code
 //
 void setup(){
-  leds.begin();
-  for(int i = 0; i < LED_COUNT; i+=17){
-    leds.setPixelColor(i,20,0,255);    
-  }
-  //leds.setPixelColor(17,20,0,255);
-  leds.show();
+  leds.begin();  
 }
 
 void loop(){
+  for(int i = 0; i < LED_COUNT; i++){
+    leds.setPixelColor(i,20,0,255);    
+  }
+  leds.show();
+  delay(100);
+  leds.clear();
+  for(int i = 0; i < LED_COUNT; i+=2){
+    leds.setPixelColor(i,20,0,255);    
+  }
+  leds.show();
+  delay(100);
+  leds.clear();
+  for(int i = 0; i < LED_COUNT; i+=17){
+    leds.setPixelColor(i,20,0,255);    
+  }
+  leds.show();
+  delay(100);
+  leds.clear();
 }
